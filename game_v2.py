013ad4235aa911557586620545ff2a16b442efc3
MIronAfanasenko <131290543+mironafanasenko@users.noreply.github.com>
2024-07-01 12:04:07 +0300

    Update game_v2.py

--- game_v2.py
+++ game_v2.py
@@ -6,7 +6,7 @@ import numpy as np
 
 
 def random_predict(number: int = 1) -> int:
-    """Рандомно угадываем число
+    """Рандомно угадываем 
 
     Args:
         number (int, optional): Загаданное число. Defaults to 1.
